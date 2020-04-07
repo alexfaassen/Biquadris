dc2370c8b746ea022ccd39b5bf6221529008c20f
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-07 12:57:32 -0400

    Commands: implemented hasSubstring()

--- command.cc
+++ command.cc
@@ -2,6 +2,8 @@
 
 std::string Command::getName() const { return name; }
 
+bool Command::hasSubstring(string playerInput) const { return playerInput == name.substr(0, playerInput.size()); }
+
 int Command::longestSubstring(String playerInput) const {
 	for (int i = 0; i < name.size(); --i) {
 		if (name.compare(0, i, playerInput)) return i + 1;
