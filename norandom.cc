7e5827cb437712e3fed7e079f2a396b8e3147954
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-11 10:08:30 -0400

    Updates to commands: will need to be reworked later when have constructors implemented

--- norandom.cc
+++ norandom.cc
@@ -6,9 +6,10 @@ using namespace std;
 
 void NoRandom::run(GameState &game, const int times) const override {
 	if (player.getLevel() == 3 || player.getLevel() == 4) {
+		istream &in = game.getStream();
 		string s;
-		cin >> s;
-		ifstream *file{s};
+		in >> s;
+		ifstream *file{new s};
 		game.getActivePlayer().setFileInput(file);
 	}
 }
