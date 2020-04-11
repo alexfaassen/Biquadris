dd78810a26866132cba17f200d35e4d483b594d0
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-11 10:24:07 -0400

    Implemented effects: will need to be reworked later when constructors are all written

--- blindeffect.cc
+++ blindeffect.cc
@@ -2,8 +2,21 @@
 
 void BlindEffect::notify(const Event currEvent, char[][] &boardPrint) override {
 	if (currEvent != endTurn) {
-		// TODO board blind
+		bool blindCol = 0;
+		for (int c = 0; c < 11; ++c) {
+			if (c >= 2 && c < 9) {
+				blindCol = 1;
+			} else {
+				blindCol = 0;
+			}
+			// Blind board
+			for (int r = 0; r < 15; ++r) {
+				if (blindCol || (r >= 2 && r < 12)) {
+					char[c][r] = '?';
+				}
+			}
+		}
 	} else {
-		// TODO board unblind
+		this->kill();
 	}
 }
--- centredropeffect.cc
+++ centredropeffect.cc
@@ -1,11 +1,12 @@
 #include "centredropeffect.h"
 
 void CentreDropEffect::notify(const Event currEvent, const int linesCleared) override {
-	if (currEvent == afterMove) {
-		if (linesCleared != 0) {
+	if (currEvent == endTurn) {
+		if (linesCleared > 0) {
 			blocksSinceCleared = 0;
 		} else if (blocksSinceCleared == 4) {
-			// TODO centre drop
+			player->forceTile(Tile(1, 0, '*', 7, 0));
+			blocksSinceCleared = 0;
 		} else {
 			++blocksSinceCleared;
 		}
