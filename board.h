f684d2e1ca95ea697702893525522628e4a094bf
gglfarra <gglfarra@student.cs.uwaterloo.ca>
2020-04-15 14:53:23 -0400

    Board, Block, and Tile

--- board.h
+++ board.h
@@ -1,2 +1,28 @@
-new file
+#ifdef _BOARD_H_
+#define _BOARD_H_
+#include <vector>
+#include "block.h"
+#include "tile.h"
+
+class Board {
+	Block *currentBlock;
+	Block *nextBlock;
+	Tiles immobileTiles[15][11];
+	vector <Block *> placed;
+	
+	public:
+	enum Direction {left, right, down}
+	int eotClean(int *score);
+	void changeCurrent(Block *cur);
+	void setNext(Block *nex);
+	int moveCurrent(Direction, int amount, bool );
+	void clockwiseCurrent();
+	void counterclockwiseCurrent();
+	void dropCurrent();
+	bool isBlocked(int x, int y);
+	char[][] renderCharArray();
+}
+
+#endif
+
 
