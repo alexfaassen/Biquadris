4dbe5f55a3005c33ce1ca04224cabb6d15682ec3
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-15 19:13:16 -0400

    Updated effects/observers to support a notify(Event, vector<vector<char>> &) method

--- blindeffect.h
+++ blindeffect.h
@@ -6,7 +6,7 @@ class BlindEffect : public Effect {
 	public:
 	BlindEffect(Player *player, bool alive) : Effect(player, alive) {}
 
-	void notify(const Event currEvent, char[][] &boardPrint) override;
+	void notify(const Event currEvent, vector<vector<char>> &boardPrint) override;
 };
 
 #endif
