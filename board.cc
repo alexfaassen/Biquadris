b198e7c44c0e1025f219172f13def1405c5122d0
RickyRister <ricky.rister.wang@gmail.com>
2020-04-17 18:01:50 -0400

    fixed typo

--- board.cc
+++ board.cc
@@ -106,7 +106,7 @@ void Board::dropCurrent() {
 
 bool Board::isBlocked(int deltaX, int deltaY){
 	for (int i = 0; i < 4; ++i) {
-		if (!isEmpty(currentBlock->getTiles[i].getX(), currentBlock->getTiles()[i].getY()) return true;
+		if (!isEmpty(currentBlock->getTiles[i].getX(), currentBlock->getTiles()[i].getY())) return true;
 	}
 	return false;
 }
