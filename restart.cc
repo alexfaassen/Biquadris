a3673b1afc61db66e1d4d01dcbc32b0243005575
Graeme Farrand <gglfarrand@gmail.com>
2020-04-18 14:14:34 -0400

    Lines refactor

--- restart.cc
+++ restart.cc
@@ -1,4 +1,6 @@
 #include "restart.h"
 #include "gamestate.h"
 
-void Restart::run(GameState &game, const int times) const { game.restart(); }
+void Restart::run(GameState &game, const int times) const {
+    game.restart();
+}
