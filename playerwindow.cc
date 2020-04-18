d6ac962a46bd73138f31f79047381c3b593a3c8e
RickyRister <ricky.rister.wang@gmail.com>
2020-04-18 16:20:10 -0400

    fixed default constructors

--- playerwindow.cc
+++ playerwindow.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 PlayerWindow::PlayerWindow(){}
 
-PlayerWindow::PlayerWindow(Xwindow* window, int offsetX, int offsetY, int width = 200, int height = 400)
+PlayerWindow::PlayerWindow(Xwindow* window, int offsetX, int offsetY, int width, int height)
 : window{window}, offsetX{offsetX}, offsetY{offsetY}, width{width}, height{height} {
     if(window){
         thisHasWindow = true;
