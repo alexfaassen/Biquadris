b73be4e507063871515b9fe0703bf5632f2e91ba
RickyRister <ricky.rister.wang@gmail.com>
2020-04-17 23:06:29 -0400

    fixed atoi in gamestate

--- gamestate.cc
+++ gamestate.cc
@@ -101,7 +101,7 @@ bool GameState::beginReadLoop(){
     while (getStream() >> s) {
         int multiplier = 1;
         if(isdigit(s[0])){          // test if s starts with an int
-            multiplier = atoi(s);   // http://www.cplusplus.com/reference/cstdlib/atoi/
+            multiplier = atoi(s.c_str());   // http://www.cplusplus.com/reference/cstdlib/atoi/
 
             // clear all digits from front of string
             while(isdigit(s[0])){
