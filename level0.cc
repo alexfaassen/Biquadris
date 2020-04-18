1afc0e6aad3bc4898015b5c2c1ebad1a74af5045
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-18 13:12:53 -0400

    typo

--- level0.cc
+++ level0.cc
@@ -7,9 +7,9 @@ Level0::Level0(const Level &other) : Level(other) { identifier = 0; }
 
 Level0::Level0(const int playerSide, int identifier, bool customSeed, int seed, std::ifstream *file) : Level(playerSide, identifier, customSeed, seed, file) {
 	if (playerSide == 1) {
-		file = new ifstream{"biquadris_sequence2.txt"};
+		file = new std::ifstream{"biquadris_sequence2.txt"};
 	} else {
-		file = new ifstream{"biquadris_sequence1.txt"};
+		file = new std::ifstream{"biquadris_sequence1.txt"};
 	}
 }
 
