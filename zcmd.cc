17ea1ab572755d57a5f631cab6edcaac62fee767
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-18 13:25:36 -0400

    typo

--- zcmd.cc
+++ zcmd.cc
@@ -1,3 +1,3 @@
 #include "zcmd.h"
 
-void ZCmd::run(GameState &game, const int times) const { game.getActivePlayer().changeCurrentBlock(new Block('Z', game.getActivePlayer().isLevel())); }
+void ZCmd::run(GameState &game, const int times) const { game.getActivePlayer().changeCurrentBlock('Z'); }
