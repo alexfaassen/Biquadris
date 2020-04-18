f21dc77caae159bd65fa6ca5970b2f2e75d9e1da
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-18 14:32:46 -0400

    typos

--- heavyeffect.cc
+++ heavyeffect.cc
@@ -1,6 +1,6 @@
 #include "heavyeffect.h"
 
-void HeavyEffect::notify(const Event currEvent, const int clearedLines) {
+void HeavyEffect::notify(const Event currEvent, const int linesCleared) {
 	if (currEvent == afterMove){
 		int moved = player->moveBlock(Down, numDown, 0);
 		if(moved < numDown){
