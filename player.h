8257b7651847baa3e56ca38e603c52fc445eed04
RickyRister <ricky.rister.wang@gmail.com>
2020-04-18 20:48:08 -0400

    fixed isLevel

--- player.h
+++ player.h
@@ -37,7 +37,7 @@ class Player {
     ~Player();
 
     //accessors
-    int isLevel() {return level ? level->getIdentifier() : 0;};
+    int isLevel();
     int getSide() {return side;};
     int getScore() {return score;};
     int incScore(int n) {score += n; return score;};
