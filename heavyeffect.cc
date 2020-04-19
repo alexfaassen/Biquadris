6a4fb1305971a04a5f7a5dc4d85de1e7cf935bea
RickyRister <ricky.rister.wang@gmail.com>
2020-04-19 19:29:14 -0400

    minor correction

--- heavyeffect.cc
+++ heavyeffect.cc
@@ -5,7 +5,7 @@ void HeavyEffect::notify(const Event currEvent, const int linesCleared) {
 	if (currEvent == afterMove){
 		int moved = player->moveBlock(Down, numDown, false);
 		if(moved < numDown){
-			player->drop(false);
+			player->drop(1, false);
 			player->endTurn();
 		}
 	} else if (currEvent == onTurnEnd) {
