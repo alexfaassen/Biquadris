71192c9b9afd1b2b87baea62f94a1d1e9bcaa9da
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-19 16:24:23 -0400

    test

--- norandom.cc
+++ norandom.cc
@@ -6,11 +6,9 @@
 #include <fstream>
 
 void NoRandom::run(GameState &game, const int times) const {
-	if (game.getActivePlayer().isLevel() == 3 || game.getActivePlayer().isLevel() == 4) {
-		std::istream &in = game.getStream();
-		std::string s;
-		in >> s;
-		std::ifstream *newFile = new std::ifstream{s};
-		game.getActivePlayer().setFileInput(newFile);
-	}
+	std::istream &in = game.getStream();
+	std::string s;
+	in >> s;
+	std::ifstream *newFile = new std::ifstream{s};
+	game.getActivePlayer().setFileInput(newFile);
 }
