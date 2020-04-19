8de79586a54d464840b1435d79acc750b79534ee
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-19 12:57:39 -0400

    test

--- sequence.cc
+++ sequence.cc
@@ -9,6 +9,6 @@ void Sequence::run(GameState &game, const int times) const {
 	std::istream &in = game.getStream();
 	std::string fileName;
 	in >> fileName;
-	std::ifstream newFile{fileName};
-	game.pushToStreams(newFile);
+	std::ifstream *newFile = new ifstream{fileName};
+	game.pushToStreams(*newFile);
 }
