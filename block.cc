ff3d41f587f7f8bf8c3c2adf778579f2bc7747cc
Graeme Farrand <gglfarrand@gmail.com>
2020-04-20 18:33:49 -0400

    testing

--- block.cc
+++ block.cc
@@ -2,75 +2,79 @@
 #include <iostream>
 #include "block.h"
 #include "tile.h"
-#include "blocktilewrapper.h"
 
 using namespace std;
 
 void Block::iBlock(int initX, int initY) {
 	deleteTiles();
 	//cout << "test: in iBlock ctor, initX = "<< initX << ", initY = " << initY << endl;
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('I', initX + 0, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('I', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('I', initX + 2, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('I', initX + 3, initY - 0)));
+	tiles.emplace_back(new Tile('I', initX + 0, initY - 0));
+	tiles.emplace_back(new Tile('I', initX + 1, initY - 0));
+	tiles.emplace_back(new Tile('I', initX + 2, initY - 0));
+	tiles.emplace_back(new Tile('I', initX + 3, initY - 0));
 }
 
 void Block::jBlock(int initX, int initY) {
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('J', initX + 0, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('J', initX + 0, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('J', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('J', initX + 2, initY - 0)));
+	tiles.emplace_back(new Tile('J', initX + 0, initY - 1));
+	tiles.emplace_back(new Tile('J', initX + 0, initY - 0));
+	tiles.emplace_back(new Tile('J', initX + 1, initY - 0));
+	tiles.emplace_back(new Tile('J', initX + 2, initY - 0));
 }
 
 void Block::lBlock(int initX, int initY) {
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('L', initX + 0, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('L', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('L', initX + 2, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('L', initX + 2, initY - 1)));
+	tiles.emplace_back(new Tile('L', initX + 0, initY - 0));
+	tiles.emplace_back(new Tile('L', initX + 1, initY - 0));
+	tiles.emplace_back(new Tile('L', initX + 2, initY - 0));
+	tiles.emplace_back(new Tile('L', initX + 2, initY - 1));
 }
 
 void Block::oBlock(int initX, int initY) {
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('O', initX + 0, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('O', initX + 0, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('O', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('O', initX + 1, initY - 1)));
+	tiles.emplace_back(new Tile('O', initX + 0, initY - 0));
+	tiles.emplace_back(new Tile('O', initX + 0, initY - 1));
+	tiles.emplace_back(new Tile('O', initX + 1, initY - 0));
+	tiles.emplace_back(new Tile('O', initX + 1, initY - 1));
 }
 
 void Block::sBlock(int initX, int initY) {
+	//cout << "test: deleteTiles" << endl;
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('S', initX + 0, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('S', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('S', initX + 1, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('S', initX + 2, initY - 1)));
+	//cout << "test: newtile 0" << endl;
+	tiles.emplace_back(new Tile('S', initX + 0, initY - 0));
+	//cout << "test: newtile 1" << endl;
+	tiles.emplace_back(new Tile('S', initX + 1, initY - 0));
+	//cout << "test: newtile 2" << endl;
+	tiles.emplace_back(new Tile('S', initX + 1, initY - 1));
+	//cout << "test: newtile 3" << endl;
+	tiles.emplace_back(new Tile('S', initX + 2, initY - 1));
 }
 
 void Block::zBlock(int initX, int initY) {
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('Z', initX + 0, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('Z', initX + 1, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('Z', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('Z', initX + 2, initY - 0)));
+	tiles.emplace_back(new Tile('Z', initX + 0, initY - 1));
+	tiles.emplace_back(new Tile('Z', initX + 1, initY - 1));
+	tiles.emplace_back(new Tile('Z', initX + 1, initY - 0));
+	tiles.emplace_back(new Tile('Z', initX + 2, initY - 0));
 }
 
 void Block::tBlock(int initX, int initY) {
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('T', initX + 0, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('T', initX + 1, initY - 1)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('T', initX + 1, initY - 0)));
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('T', initX + 2, initY - 1)));
+	tiles.emplace_back(new Tile('T', initX + 0, initY - 1));
+	tiles.emplace_back(new Tile('T', initX + 1, initY - 1));
+	tiles.emplace_back(new Tile('T', initX + 1, initY - 0));
+	tiles.emplace_back(new Tile('T', initX + 2, initY - 1));
 }
 
 void Block::tileDropBlock(int initX, int initY) {
 	deleteTiles();
-	tiles.emplace_back(BlockTilewrapper(window, new Tile('*', initX, initY)));
+	tiles.emplace_back(new Tile('*', initX, initY));
 }
 
 Block::Block(char type, int initLevel, int heavy, int x, int y):
 type{type}, initLevel{initLevel}, heaviness{heavy} {
-	cout << "Block ctor is run with " << string(1,type) << endl;
+	//cout << "Block ctor is run with " << string(1,type) << endl;
 	if(type == '*') tileDropBlock(x, y);
 	else if(type == 'I') iBlock(x, y);
 	else if(type == 'J') jBlock(x, y);
@@ -79,7 +83,6 @@ type{type}, initLevel{initLevel}, heaviness{heavy} {
 	else if(type == 'S') sBlock(x, y);
 	else if(type == 'Z') zBlock(x, y);
 	else tBlock(x, y);
-	cout << "test: Block constructed with tile size = " << tiles.size() << endl;
 }
 
 Block::~Block(){
@@ -87,15 +90,13 @@ Block::~Block(){
 }
 
 void Block::move(int deltaX, int deltaY) {
-	undraw();
-	for(auto &t : tiles) {
-		t->setXY(t->getX() + deltaX, t->getY() + deltaY);
+	for(auto t : tiles) {
+		t->setX(t->getX() + deltaX);
+		t->setY(t->getY() + deltaY);
 	}
-	draw();
 }
 
 void Block::clockwise() {
-  undraw();
 	cout << "before clockwise" <<endl;
 	for(auto t : tiles) {
 		cout << "(" << t->getX() << ", " << t->getY() << ") ";
@@ -103,7 +104,7 @@ void Block::clockwise() {
 	cout <<endl;
 
 	int maxX = 0, maxY = -3, minX = 11, minY = 18;
-	for(auto &t : tiles) {
+	for(auto t : tiles) {
 		if(t->getX() > maxX)maxX = t->getX();
 		if(t->getY() > maxY)maxY = t->getY();
 		if(t->getX() < minX)minX = t->getX();
@@ -111,22 +112,26 @@ void Block::clockwise() {
 	}
 
 	cout << "Bottom Left: " << maxY << ", " << minX <<endl;
-	Block rotateBlock = Block(type, 0, 0, 0, 0);
+
+
 	int maxRotateX = 0, maxRotateY = -3;
-	for(auto t : rotateBlock.tiles) {
-		if(t->getX() > maxRotateX)maxRotateX = t->getX();
-		if(t->getY() > maxRotateY)maxRotateY = t->getY();
-	}
+	// for(auto t : rotateBlock.tiles) {
+	// 	if(t->getX() > maxRotateX)maxRotateX = t->getX();
+	// 	if(t->getY() > maxRotateY)maxRotateY = t->getY();
+	// }
 	int distUp;
-	for(auto &t : rotateBlock.tiles) {
+	cout << "inverting: ";
+	for(auto t : tiles) {
 		distUp = maxRotateY - t->getY();
 		// t->setX(t->getX() - minX);
 		// t->setX(t->getY() - minY);
 		t->setY(maxRotateY + distUp);
 		t->invert();
+		cout << "(" << t->getX() << ", " << t->getY() << ") "
 		t->setY(t->getY() - maxRotateX + maxY);
 		t->setX(t->getX() + minX);
 	}
+	cout <<endl;
 
 	// for(auto t : tiles) {
 	// 	t->setX(t->getX() - minX);
@@ -172,7 +177,6 @@ void Block::clockwise() {
 	for(auto t : tiles) {
 		cout << "(" << t->getX() << ", " << t->getY() << ") ";
 	}
-	draw();
 	cout <<endl;
 }
 
@@ -180,16 +184,8 @@ void Block::counterClockwise() {
 	for(int i = 0; i < 3; i++)clockwise();
 }
 
-void Block::attachWindow(PlayerWindow* w) {
-	window = w;
-	for(auto &t : tiles){
-		t.setWindow(window);
-	}
-	draw();
-}
-
 bool Block::alive() {
-	for(auto &t : tiles){
+	for(auto t : tiles){
 		if(t->isAlive()){
 			return true;
 		}
@@ -197,15 +193,13 @@ bool Block::alive() {
 	return false;
 }
 
-bool Block::deleteTiles(){
-	if(tiles.empty()){
-		//cout << "test: deleteTiles is empty" << endl; 
-		return false;
+void Block::deleteTiles(){
+	for(auto p : tiles){
+		//cout << "test: deleting p" << endl;
+		if(p) delete p;
+		//cout << "test: deleted p" << endl;
 	}
-	undraw();
-	//cout << "test: deleteTiles with n = " << tiles.size() << endl;
 	tiles.clear();
-	return true;
 }
 
 std::string Block::printBlock() const {
@@ -213,7 +207,7 @@ std::string Block::printBlock() const {
 	string str = "";
 	for (int row = -1; row <= 0; ++row) {
 		for (int col = 0; col < 11; ++col) {
-			for (auto& t : tiles) {
+			for (auto t : tiles) {
 				if (t->getY() == row && t->getX() == col) {
 					str += string(1, type);	
 					isTile = true;
@@ -226,16 +220,4 @@ std::string Block::printBlock() const {
 		str += "\n";
 	}
 	return str;
-}
-
-void Block::draw(){
-	for(auto &t : tiles){
-		t.draw();
-	}
-}
-
-void Block::undraw(){
-	for(auto &t : tiles){
-		t.undraw();
-	}
 }
