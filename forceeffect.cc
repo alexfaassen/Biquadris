9407206811dc7b760f8dee8111113f0123383cd6
RickyRister <ricky.rister.wang@gmail.com>
2020-04-19 22:04:45 -0400

    updated forceEffect

--- forceeffect.cc
+++ forceeffect.cc
@@ -2,7 +2,7 @@
 #include "player.h"
 
 void ForceEffect::notify(const Event currEvent, const int linesCleared) {
-	if (currEvent == onTurnStart) {
+	if (currEvent == onTurnStart || currEvent == beforeMove) {
 		player->changeCurrentBlock(newBlock);
 		kill();
 	}
