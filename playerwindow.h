0dbc5269b1f8b9b5d0e435cd924908e46417b243
RickyRister <ricky.rister.wang@gmail.com>
2020-04-20 16:02:13 -0400

    colors

--- playerwindow.h
+++ playerwindow.h
@@ -24,19 +24,19 @@ class PlayerWindow{
     bool hasWindow() {return thisHasWindow;};             
 
 	// Draws a string
-	void drawString(int x, int y, std::string msg, int colour = Black);
-	void drawBigString(int x, int y, std::string msg, int colour = Black);
+	void drawString(int x, int y, std::string msg, int colour = XWindow::Black);
+	void drawBigString(int x, int y, std::string msg, int colour = XWindow::Black);
 
 	// if you use this function, make sure font exists on the undergrad environment
-        void drawStringFont(int x, int y, std::string msg, std::string font, int colour = Black);
+        void drawStringFont(int x, int y, std::string msg, std::string font, int colour = XWindow::Black);
 
 	// Draws a rectangle
-	void fillRectangle(int x, int y, int width, int height, int colour = Black);
+	void fillRectangle(int x, int y, int width, int height, int colour = XWindow::Black);
 
 	// Draw a polygon with the first vertex at (x, y) with num vertices, side length side
 	//     and rotated rotate radians. The second vertex is (x + side, y) rotated by
 	//     rotate
-	void fillPolygon(int x, int y, int num, int side, int rotate = 0, int colour = Black);
+	void fillPolygon(int x, int y, int num, int side, int rotate = 0, int colour = XWindow::Black);
 
 	// Draws a line from (x1, y1) to (x2, y2)
 	void drawLine(int x1, int y1, int x2, int y2);
@@ -48,7 +48,7 @@ class PlayerWindow{
 	void fillArc(int x, int y, int width, int height, int angle1, int angle2,
 			int colour);
 	// Draws a circle centered at (x, y) with diameter d
-	void fillCircle(int x, int y, int d, int colour = Black);
+	void fillCircle(int x, int y, int d, int colour = XWindow::Black);
 
         void showAvailableFonts();
 };
