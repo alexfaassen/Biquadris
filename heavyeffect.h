09444539b2b4c2919374e03817ddfb46d0e543b1
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-19 20:55:03 -0400

    test

--- heavyeffect.h
+++ heavyeffect.h
@@ -1,12 +1,15 @@
 #ifndef HEAVYEFT_H
 #define HEAVYEFT_H
-#include "effect.h"
+#include "effect.h" 
+#include <iostream>
+
+using namespace std;
 
 class HeavyEffect : public Effect {
 	int numDown;
 
 	public:
-	HeavyEffect(int numDown = 1, bool temp = true) : Effect(temp), numDown{numDown} {}
+	HeavyEffect(bool temp = true, int numDown = 1) : Effect(temp), numDown{numDown} {}
 
 	void notify(const Event currEvent, const int linesCleared) override;
 };
