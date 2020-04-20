ae3c6f9c722eb644dcd3a4d4697b3f8d65bea9fa
alexfaassen <adrfaass@AlexDellXPS15.localdomain>
2020-04-19 20:35:02 -0400

    test

--- heavyeffect.cc
+++ heavyeffect.cc
@@ -2,7 +2,9 @@
 #include "player.h"
 
 void HeavyEffect::notify(const Event currEvent, const int linesCleared) {
+	cout << "test: heavyeffect notify() is called" << endl;
 	if (currEvent == afterMove) {
+		cout << "test: heavyeffect is running, afterMove" << endl;
 		int moved = player->moveBlock(Down, numDown, false);
 		if(moved < numDown){
 			player->drop(1, false);
