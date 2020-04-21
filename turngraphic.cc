b203d07d7dbb003f8e5a4afaa4139ddd5e28c02c
RickyRister <ricky.rister.wang@gmail.com>
2020-04-21 16:49:26 -0400

    test

--- turngraphic.cc
+++ turngraphic.cc
@@ -10,7 +10,7 @@ void TurnGraphic::notify(const Event currEvent, int active){
         //TODO: draw turngraphic
         if(active){
             //window->fillRectangle(180, 442,  30,  30, PlayerWindow::Yellow);
-            window->fillPolygon(220, 472, 3, 40, (1) * M_PI + M_PI, PlayerWindow::White);
+            window->fillPolygon(220, 472, 3, 40, 0, PlayerWindow::White);
         } else {
             window->fillRectangle(180, 435,  40,  40, PlayerWindow::Black);
         }
