891615b4834911f9eb16934adb5c107165aea9bd
RickyRister <ricky.rister.wang@gmail.com>
2020-04-21 16:27:03 -0400

    turngraphic refining

--- turngraphic.cc
+++ turngraphic.cc
@@ -1,6 +1,7 @@
 #include "turngraphic.h"
 #include "player.h"
 #include "playerwindow.h"
+#include <cmath>
 
 void TurnGraphic::notify(const Event currEvent, int active){
     // active acts like a bool.  0 is false, 1 is true
@@ -9,9 +10,9 @@ void TurnGraphic::notify(const Event currEvent, int active){
         //TODO: draw turngraphic
         if(active){
             //window->fillRectangle(180, 442,  30,  30, PlayerWindow::Yellow);
-            window->fillPolygon(180, 442, 3, 20, 0, PlayerWindow::White);
+            window->fillPolygon(180, 442, 3, 30, M_PI , PlayerWindow::White);
         } else {
-            window->fillRectangle(180, 442,  30,  30, PlayerWindow::Black);
+            window->fillRectangle(160, 435,  40,  40, PlayerWindow::Black);
         }
     }
 }
