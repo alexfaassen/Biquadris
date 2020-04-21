611179438c3047a8451ad89a7f71d4ca3544f4a2
RickyRister <ricky.rister.wang@gmail.com>
2020-04-21 03:56:19 -0400

    tweaks

--- scoregraphic.cc
+++ scoregraphic.cc
@@ -2,6 +2,11 @@
 #include "player.h"
 #include "playerwindow.h"
 
+using namespace std;
+
 void ScoreGraphic::notify(const Event currEvent, Move m){
-       window->drawString(window->getOffsetX() + 221, 30, std::to_string(player->getScore()), PlayerWindow::Black); 
+       if(currEvent == onScoreChange){
+              //TODO: draw white square over old score
+              window->drawString(221, 30, to_string(player->getScore()), PlayerWindow::Black); 
+       }
 }
