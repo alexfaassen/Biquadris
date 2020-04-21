3348d634007c7a320e741e8899eb94b53f667ec9
adrfaass <adrfaass@student.cs.uwaterloo.ca>
2020-04-20 23:37:46 -0400

    test

--- nextblockgraphic.cc
+++ nextblockgraphic.cc
@@ -6,10 +6,9 @@
 
 void NextBlockGraphic::notify(const Event currEvent, const char c){	
 	window->fillRectangle(1, 442, 84, 483, PlayerWindow::White);
-	Block *tempBlock = new Block(c, -1, 0, 1, 442);
-	std::cout << "tempBlock.getType() = " << tempBlock->getType() << std::endl;
-	tempBlock->attachWindow(window);
-	tempBlock->draw();
-	delete tempBlock;
+	Block tempBlock(c, -1, 0, 1, 442);
+	std::cout << "tempBlock.getType() = " << tempBlock.getType() << std::endl;
+	tempBlock.attachWindow(window);
+	tempBlock.draw();
 	std::cout << "tempBlock.draw() happening" << std::endl;
 }
