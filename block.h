52d1d9185ac0c88d4841af84622b2af2f82b52e3
adrfaass <adrfaass@student.cs.uwaterloo.ca>
2020-04-20 20:19:47 -0400

    moved things in block & tile headers

--- block.h
+++ block.h
@@ -13,11 +13,6 @@ class Block {
 	std::vector<BlockTilewrapper> tiles;
 	PlayerWindow* window = nullptr;
 
-	public:
-	//constructors
-	Block(char type, int initLevel, int heavy = 0, int x = 0, int y = 0);
-	~Block();
-
 	//set block types
 	void iBlock(int x, int y);
 	void jBlock(int x, int y);
@@ -28,6 +23,11 @@ class Block {
 	void tBlock(int x, int y);
 	void tileDropBlock(int x, int y);
 
+	public:
+	//constructors
+	Block(char type, int initLevel, int heavy = 0, int x = 0, int y = 0);
+	~Block();
+
 	//movement
 	void move(int deltaX, int deltaY);
 	void clockwise();
@@ -37,13 +37,15 @@ class Block {
 	char getType() const {return type;};
 	int getInitLevel() const {return initLevel;};
 	std::vector<BlockTilewrapper>& getTiles() {return tiles;};
-	std::string printBlock() const;
 	int getHeaviness() const {return heaviness;};
 	void attachWindow(PlayerWindow* w = nullptr);
 
 	//maintainence
 	bool alive();
 	bool deleteTiles();
+
+	//text
+	std::string printBlock() const;
 
 	//drawing
 	void draw();
--- tile.h
+++ tile.h
@@ -11,16 +11,18 @@ class Tile {
 	Tile(char letter = ' ', int x = 0, int y = 0) : letter{letter}, x{x}, y{y} {};
 	Tile(Tile& other) : Tile(other.letter, other.x, other.y) {};
 	~Tile();
+
+	//accessors
 	char getLetter() const;
 	void setLetter(char newLetter);
 	int getX() const;
 	int getY() const;
-	bool isAlive() const {return alive;};
-	void kill();
 	void setX(const int newX);
 	void setY(const int newY);
 	void setXY(const int newX, const int newY);
 	void invert();
+	bool isAlive() const {return alive;};
+	void kill();
 };
 
 #endif
