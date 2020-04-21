6a4caebe050bb7dc9bc11386ee61fd887e1c4b5b
Graeme Farrand <gglfarrand@gmail.com>
2020-04-21 15:12:36 -0400

    forceEffect notify debug message

--- forceeffect.cc
+++ forceeffect.cc
@@ -1,10 +1,13 @@
 #include "forceeffect.h"
 #include "player.h"
 
+#include <stdio.h>
+
 using namespace std;
 
 void ForceEffect::notify(const Event currEvent, const Move currMove) {
 	if (currEvent == onTurnStart || currEvent == onSAapplied) {
+		cout << "ForceEffect newBlock: " << newBlock <<endl;
 		player->changeCurrentBlock(newBlock);
 		kill();
 	}
