23d8fdbf8b17a5d3a905330d852bfe43b90df93f
adrfaass <adrfaass@student.cs.uwaterloo.ca>
2020-04-20 21:19:28 -0400

    test

--- levelgraphic.cc
+++ levelgraphic.cc
@@ -3,5 +3,5 @@
 #include "playerwindow.h"
 
 void LevelGraphic::notify(const Event currEvent, Move m){
-       window->drawString(window->getOffsetX() + 205, 10, "Level: " + player->isLevel(), PlayerWindow::Black);
+       window->drawString(window->getOffsetX() + 205, 10, to_string(player->isLevel()), PlayerWindow::Black);
 }
