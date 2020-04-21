9430a969d3cede2133c41d0227e2ba42f5a0e9e9
Graeme Farrand <gglfarrand@gmail.com>
2020-04-21 14:59:11 -0400

    debiug messages

--- force.cc
+++ force.cc
@@ -4,10 +4,12 @@
 
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 
 void Force::run(GameState &game, const int times) const {
 	char b;
 	game.readFromStream(b);
+	cout << "force::run New Block: " << b <<endl;
 	game.getNAP().pushToObservers(new ForceEffect(b));
 	if(game.getNAP().getInputState() != END_TURN) game.getNAP().notifyObservers(onSAapplied);
 }
--- forceeffect.cc
+++ forceeffect.cc
@@ -1,12 +1,10 @@
 #include "forceeffect.h"
 #include "player.h"
-#include <ostream.h>
 
 using namespace std;
 
 void ForceEffect::notify(const Event currEvent, const Move currMove) {
 	if (currEvent == onTurnStart || currEvent == onSAapplied) {
-		cout << "newBlock: " << newBlock <<endl;
 		player->changeCurrentBlock(newBlock);
 		kill();
 	}
