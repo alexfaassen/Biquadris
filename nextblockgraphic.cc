9050fb51ad126c858c2a5121889e547da9a3d4b1
adrfaass <adrfaass@student.cs.uwaterloo.ca>
2020-04-20 22:51:11 -0400

    test

--- nextblockgraphic.cc
+++ nextblockgraphic.cc
@@ -1,7 +1,11 @@
 #include "nextblockgraphic.h"
 #include "player.h"
 #include "playerwindow.h"
+#include "block.h"
 
 void NextBlockGraphic::notify(const Event currEvent, const char c){	
-	// TODO
+	window->fillRectangle(1, 443, 84, 484);
+	Block tempBlock(c, -1, 1, 443);
+	tempBlock.attachWindow(window);
+	tempBlock.draw();
 }
--- nextblockgraphic.d
+++ nextblockgraphic.d
@@ -1,3 +1,3 @@
 nextblockgraphic.o: nextblockgraphic.cc nextblockgraphic.h \
  graphicsobserver.h observer.h event.h move.h player.h inputstate.h \
- direction.h playerwindow.h
+ direction.h playerwindow.h block.h
